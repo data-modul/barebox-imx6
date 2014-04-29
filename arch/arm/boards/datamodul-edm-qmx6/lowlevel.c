c55bf2966d40c5c2c98f58bdefc756bd94c1b529
Silvio Fricke <silvio.fricke@gmail.com>
2014-04-29 15:14:11 +0200

    ARM: i.MX6: add edmqmx6-dl support
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>
    
    Conflicts:
    	arch/arm/dts/Makefile

--- arch/arm/boards/datamodul-edm-qmx6/lowlevel.c
+++ arch/arm/boards/datamodul-edm-qmx6/lowlevel.c
@@ -43,6 +43,7 @@ static void uart_init(void)
 }
 
 extern char __dtb_imx6q_dmo_edmqmx6_start[];
+extern char __dtb_imx6dl_dmo_edmqmx6_start[];
 
 ENTRY_FUNCTION(start_imx6q_realq7, r0, r1, r2)
 {
@@ -56,6 +57,22 @@ ENTRY_FUNCTION(start_imx6q_realq7, r0, r1, r2)
 	arm_early_mmu_cache_invalidate();
 
 	fdt = __dtb_imx6q_dmo_edmqmx6_start - get_runtime_offset();
+
+	barebox_arm_entry(sdram, SZ_1G, fdt);
+}
+
+ENTRY_FUNCTION(start_imx6dl_realq7, r0, r1, r2)
+{
+	unsigned long sdram = 0x10000000;
+	void *fdt;
+
+	imx6_cpu_lowlevel_init();
+
+	arm_setup_stack(0x00920000 - 8);
+
+	arm_early_mmu_cache_invalidate();
+
+	fdt = __dtb_imx6dl_dmo_edmqmx6_start - get_runtime_offset();
 
 	barebox_arm_entry(sdram, SZ_1G, fdt);
 }
