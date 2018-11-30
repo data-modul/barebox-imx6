8845ed4eeefb1dbc52474b15c44f71bc61311aac
Elmar Albert <ealbert@data-modul.com>
2018-11-30 10:09:22 +0100

    lowlevel: added support and recovery image for DRAM 2G_32bit
    Signed-off-by: Elmar Albert <ealbert@data-modul.com>

--- arch/arm/boards/dmo-imx6-ppc/lowlevel.c
+++ arch/arm/boards/dmo-imx6-ppc/lowlevel.c
@@ -529,6 +529,22 @@ ENTRY_FUNCTION(start_dmo_imx6_ppc, r0, r1, r2)
  *
  */
 
+ENTRY_FUNCTION(start_dmo_imx6dl_ppc_2G_32bit, r0, r1, r2)
+{
+	unsigned long sdram = 0x08000000;
+	void *fdt;
+
+	imx6_cpu_lowlevel_init();
+
+	arm_setup_stack(STACK_SDL - 8); /* according to IMXSDLRM.pdf: 8.4.1 Internal ROM /RAM memory map */
+
+	arm_early_mmu_cache_invalidate();
+
+	fdt = __dtb_imx6dl_dmo_ppc_start - get_runtime_offset();
+
+	barebox_arm_entry(sdram, SZ_512M, fdt);
+}
+
 ENTRY_FUNCTION(start_dmo_imx6dl_ppc_4G_32bit, r0, r1, r2)
 {
 	unsigned long sdram = 0x10000000;
